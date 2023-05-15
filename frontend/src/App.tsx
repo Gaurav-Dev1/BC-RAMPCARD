0952d1710215c08fbc19b9005db36fe463329f0e
Gaurav Shukla <gaurav.shukl@zemosolabs.com>
2023-05-16 00:53:09 +0530

    feat: text select implemented without placeholder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import theme from './Theme/theme'
-import { ThemeProvider } from '@mui/material'
-import TextSelect from './components/molecules/TextSelect'
+import { SelectChangeEvent, ThemeProvider } from '@mui/material'
+import DropDown from './components/molecules/TextSelect'
+import { QUICKBOOKS_CATEGORY_OPTION } from './constants/constant'
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <div style={{marginTop: '20px'}}>
-        <TextSelect />
+        <DropDown value={''} onChange={function (event: SelectChangeEvent<unknown>): void {
+          throw new Error('Function not implemented.')
+        } } items={QUICKBOOKS_CATEGORY_OPTION}  />
       </div>
     </ThemeProvider>
   )
