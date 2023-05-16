196f222ab0f37c0910f3670896fe1710b7fdc1cc
Gaurav Shukla <gaurav.shukl@zemosolabs.com>
2023-05-16 11:40:23 +0530

    feat: dropdown component implemented

--- frontend/src/constants/constant.ts
+++ frontend/src/constants/constant.ts
@@ -3,3 +3,4 @@ export const TYPOGRAPHY_NEGATIVE_TEST_TEXT = 'negative-test'
 export const ICONBUTTON_TESTID = 'icon-button'
 export const ICON_DATA_TESTID = 'icon-comp'
 export const QUICKBOOKS_CATEGORY_OPTION = ['Expense', 'Travel','Travel meals', 'Hotels']
+export const QUICKBOOKS_CATEGORY_DROPDOWN_PLACEHOLDER = 'Choose one'
