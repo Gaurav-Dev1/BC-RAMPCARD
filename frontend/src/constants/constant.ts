ac39eefb75c14f66d7d2152f964dbe5977b5f4b0
Gaurav Shukla <gaurav.shukla@zemosolabs.com>
2023-05-16 17:50:31 +0530

    feat: icon atom implemented (#7)
    Co-authored-by: Gaurav Shukla <gaurav.shukl@zemosolabs.com>

--- frontend/src/constants/constant.ts
+++ frontend/src/constants/constant.ts
@@ -2,3 +2,4 @@ export const TYPOGRAPHY_POSITIVE_TEST_TEXT = 'typography'
 export const TYPOGRAPHY_NEGATIVE_TEST_TEXT = 'negative-test'
 export const ICON_DATA_TESTID = 'icon-comp'
 export const SEARCH_CARDS = 'Search cards'
+
