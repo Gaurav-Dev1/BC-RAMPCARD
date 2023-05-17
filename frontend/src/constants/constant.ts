e9c46a8782f4ca6b6845b7441e9f2d54af522d18
Gaurav Shukla <gaurav.shukla@zemosolabs.com>
2023-05-17 09:07:39 +0530

    feat: atom-iconbutton (#8)
    Co-authored-by: Gaurav Shukla <gaurav.shukl@zemosolabs.com>

--- frontend/src/constants/constant.ts
+++ frontend/src/constants/constant.ts
@@ -2,4 +2,4 @@ export const TYPOGRAPHY_POSITIVE_TEST_TEXT = 'typography'
 export const TYPOGRAPHY_NEGATIVE_TEST_TEXT = 'negative-test'
 export const ICON_DATA_TESTID = 'icon-comp'
 export const SEARCH_CARDS = 'Search cards'
-
+export const ICONBUTTON_TESTID = 'icon-button'
